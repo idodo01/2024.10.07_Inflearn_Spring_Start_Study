51fa9fbcaca262d2b434602409f98b52b9ffeefc
IDO <susususu07@naver.com>
2024-11-01 14:16:48 +0900

    Add files via upload

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -19,22 +19,24 @@ public class HelloController {
 
     // http://localhost:8080/hello-mvc?name="홍길동"
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String nameStr, Model model) {
+    // required=false 하면, name을 입력하지 않아도 정상 작동됨
+    public String helloMvc(@RequestParam(value="name", required=false) String nameStr, Model model) {
         model.addAttribute("name", nameStr);
         return "hello-template"; // hello-template.html
     }
 
     @GetMapping("hello-string")
     @ResponseBody // html의 body가 아닌, http의 body부에 해당 내용을 직접 넣겠다
-    // required=false 하면, name을 입력하지 않아도 정상 작동됨
-    public String helloString(@RequestParam(value="name", required=false) String nameStr) {
+    // 해당 페이지에서 소스보기를 했을 때
+    // string을 보내면, string만 딱 나옴 (원래는 html 다 나왔지)
+    public String helloString(@RequestParam(name) String nameStr) {
         return "텍스트만 바로 출력할 수 있다!"; // html이 아닌 텍스트가 화면에 바로 출력됨
         // ex. "hello 홍길동"
     }
 
-
     @GetMapping("hello-api")
     @ResponseBody
+    // 클래스를 이용
     public HelloClass helloApi(@RequestParam("name") String nameStr1) {
         HelloClass hello = new HelloClass(); // 객체생성
         hello.setName(nameStr1);
