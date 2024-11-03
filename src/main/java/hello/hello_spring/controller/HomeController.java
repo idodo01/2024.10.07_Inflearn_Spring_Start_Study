b2badaa84c0c5791d94bfb6d6b0ba0f2834bddbd
IDO <susususu07@naver.com>
2024-11-03 20:29:33 +0900

    Add files via upload

--- src/main/java/hello/hello_spring/controller/HomeController.java
+++ src/main/java/hello/hello_spring/controller/HomeController.java
@@ -8,6 +8,7 @@ public class HomeController {
 
     // http://localhost:8080/
     // Mapping 된 것이 없으면 resource > static 폴더의 html 파일로 접근함
+    // 하지만 resource > template에 home이 있기에 정상 접근됨!
     @GetMapping("/")
     public String home() {
         return "home";
